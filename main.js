43cb4b1668b9a91ffd0672d615264c5abbce5ed7
Aleksandra Denisova <tisudd22aleden@innovationsgymnasiet.se>
2022-10-28 18:37:48 +0200

    my first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let count = 0;
+let countEl = document.getElementById("count-el");
+let buttons = document.querySelectorAll("button");
+
+function increce() {
+  count += 1;
+  countEl.innerText = count;
+}
+
+function save() {
+  for (const button of buttons) {
+    button.disabled = true;
+  }
+}
+
+function reset() {
+  count = 0;
+  countEl.innerText = count;
+}
+
+function to_another() {
+  location.href = "index2.html";
+  writeFile("clicks.text", count)
+}
